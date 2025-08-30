7bc9d481a9379fddff222e9f74754de31fb224a2
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2025-08-30 15:45:22 +0700

    [STYLE] add chart content, transaction history on the main page

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,6 @@ themeToggleBtn.addEventListener("click", function () {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("color-theme", "light");
     }
-
   } else {
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
@@ -33,6 +32,78 @@ themeToggleBtn.addEventListener("click", function () {
     }
   }
 });
+
+const options = {
+  chart: {
+    height: "100%",
+    maxWidth: "100%",
+    type: "area",
+    fontFamily: "Inter, sans-serif",
+    dropShadow: {
+      enabled: false,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    enabled: true,
+    x: {
+      show: false,
+    },
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      opacityFrom: 0.55,
+      opacityTo: 0,
+      shade: "oklch(82.8% 0.111 230.318)",
+      gradientToColors: ["oklch(82.8% 0.111 230.318)"],
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    width: 6,
+  },
+  grid: {
+    show: false,
+    strokeDashArray: 4,
+    padding: {
+      left: 2,
+      right: 2,
+      top: 0,
+    },
+  },
+  series: [
+    {
+      name: "New users",
+      data: [6500, 6418, 6456, 6526, 6356, 6456],
+      color: "oklch(58.8% 0.158 241.966)",
+    },
+  ],
+  xaxis: {
+    categories: ["01 February", "02 February", "03 February", "04 February", "05 February", "06 February", "07 February"],
+    labels: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: false,
+  },
+};
+
+if (document.getElementById("area-chart") && typeof ApexCharts !== "undefined") {
+  const chart = new ApexCharts(document.getElementById("area-chart"), options);
+  chart.render();
+}
 
 // humberger
 function toggleSidebar() {
